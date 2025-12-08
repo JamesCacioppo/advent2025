7bd9d711c229ea64b25bbd09d8d2e5509683f121
James Cacioppo <38083654+JamesCacioppo@users.noreply.github.com>
2025-12-08 17:46:32 +0000

    Set up typer

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from pathlib import Path
+import typer
 
-from day1 import solve1
+from day1 import solve1, solve2
 
+app = typer.Typer()
 
-def main() -> None:
-    input_path = Path(__file__).parent / "data" / "day1.txt"
+@app.command()
+def day1(input_file: str) -> None:
+    input_path = Path(__file__).parent / "data" / input_file
     part1_answer = solve1(input_path)
     print("Part 1:", part1_answer)
 
+    part2_answer = part1_answer + solve2(input_path)
+    print("Part 2:", part2_answer)
 
 if __name__ == "__main__":
-    main()
+    app()
